8da2161b10fae8f0c8480247dba9973944e3d091
Sugeng Wibowo <swibowo@bbmtek.com>
2019-02-21 02:03:42 +0700

    Initial Commit

---
+++ EverydayNormalRxSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EverydayNormalRxSwift
+//
+//  Created by Sugeng Wibowo on 21/02/19.
+//  Copyright © 2019 Sugeng Wibowo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ EverydayNormalRxSwiftTests/EverydayNormalRxSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  EverydayNormalRxSwiftTests.swift
+//  EverydayNormalRxSwiftTests
+//
+//  Created by Sugeng Wibowo on 21/02/19.
+//  Copyright © 2019 Sugeng Wibowo. All rights reserved.
+//
+
+import XCTest
+@testable import EverydayNormalRxSwift
+
+class EverydayNormalRxSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
